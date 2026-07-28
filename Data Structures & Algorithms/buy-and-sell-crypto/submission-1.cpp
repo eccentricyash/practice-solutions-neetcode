608124682e5954f199538cd888d66246e5251f9d
Yash Yadav <tooyashyadav@gmail.com>
2026-07-28 21:29:43 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size()-1;
+        int left=0,profit=0,maxprofit=0;
+        for(int right=0;right<=n;right++){
+            int profit=prices[right]-prices[left];
+            if(profit<0){
+                left=right;
+            }
+            maxprofit=max(profit,maxprofit);
+
+        }
+        return maxprofit;
+    }
+};
